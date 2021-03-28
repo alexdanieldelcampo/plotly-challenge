4f96de689ab00b05d3b8d724688d6930b1898104
Alexander del Campo <Alexander@macbook-pro.lan>
2021-03-28 13:11:48 -0700

    first try

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,41 @@
+d3.json("../data/samples.json").then((data)=> {
+    console.log(data.samples)
+
+    var sample_values = data.samples.map(row => row.sample_values)
+
+    console.log(sample_values)
+
+   
+    // console.log(objects)
+
+
+
+
+
+
+
+})
+
+//     //  Create the Traces
+//     var trace1 = {
+//       x: data.organ,
+//       y: data.survival.map(val => Math.sqrt(val)),
+//       type: "box",
+//       name: "Cancer Survival",
+//       boxpoints: "all"
+//     };
+  
+//     // Create the data array for the plot
+//     var data = [trace1];
+  
+//     // Define the plot layout
+//     var layout = {
+//       title: "Square Root of Cancer Survival by Organ",
+//       xaxis: { title: "Organ" },
+//       yaxis: { title: "Square Root of Survival" }
+//     };
+  
+//     // Plot the chart to a div tag with id "plot"
+//     Plotly.newPlot("plot", data, layout);
+//   });
+  
